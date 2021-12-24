3c4464b689909c327905d88ac69693a0c3873c33
single1709 <90201641+single1709@users.noreply.github.com>
2021-12-24 20:24:15 +0300

    Add files via upload
    v2

--- homework.py
+++ homework.py
@@ -77,7 +77,7 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        first_part: float = ((self.RATE_CALORIE_RUN_1
+        first_part: float = ((self.RATE_CALORIE_RUN1
                              * self.get_mean_speed() - self.RATE_CALORIE_RUN2))
         spent_calories: float = (first_part
                                  * self.weight / self.M_IN_KM
